6d832f5a0f6eb73713e21c18b87ab7bdff327ded
Iunave <55002444+Iunave@users.noreply.github.com>
2020-11-21 16:32:29 +0000

    Update FileReader.cpp

--- MorseEncoder_Decoder/FileReader.cpp
+++ MorseEncoder_Decoder/FileReader.cpp
@@ -13,164 +13,156 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include "FileReader.h"
 namespace MorseCodes
 {
-    inline constexpr uint16 Short{'*'};
-    inline constexpr uint16 Long{'-'};
-    inline constexpr uint16 NewWord{'|'};
-    inline constexpr uint16 Space{'&'};
+    inline constexpr int16 Short{'*'};
+    inline constexpr int16 Long{'-'};
+    inline constexpr int16 NewWord{'|'};
+    inline constexpr int16 Space{'&'};
 
-    inline constexpr Simd::uint16_8 NullChar{0, 0, 0, 0, 0, 0, 0, 0};
-    inline constexpr Simd::uint16_8 NewWordChar{NewWord, 0, 0, 0, 0, 0, 0, 0};
-    inline constexpr Simd::uint16_8 SeparateCharacterChar{Space, 0, 0, 0, 0, 0, 0, 0};
+    inline constexpr Simd::int16_8 NullChar{0, 0, 0, 0, 0, 0, 0, 0};
+    inline constexpr Simd::int16_8 NewWordChar{NewWord, 0, 0, 0, 0, 0, 0, 0};
+    inline constexpr Simd::int16_8 SeparateCharacterChar{Space, 0, 0, 0, 0, 0, 0, 0};
 
-    inline constexpr Simd::uint16_8 A{Short, Long, 0, 0, 0, 0, 0, 0};
-    inline constexpr Simd::uint16_8 B{Long, Short, Short, Short, 0, 0, 0, 0};
-    inline constexpr Simd::uint16_8 C{Long, Short, Long, Short, 0, 0, 0, 0};
-    inline constexpr Simd::uint16_8 D{Long, Short, Short, 0, 0, 0, 0, 0};
-    inline constexpr Simd::uint16_8 E{Short, 0, 0, 0, 0, 0, 0, 0};
-    inline constexpr Simd::uint16_8 F{Short, Short, Long, Short, 0, 0, 0, 0};
-    inline constexpr Simd::uint16_8 G{Long, Long, Short, 0, 0, 0, 0, 0};
-    inline constexpr Simd::uint16_8 H{Short, Short, Short, Short, 0, 0, 0, 0};
-    inline constexpr Simd::uint16_8 I{Short, Short, 0, 0, 0, 0, 0, 0};
-    inline constexpr Simd::uint16_8 J{Short, Long, Long, Long, 0, 0, 0, 0};
-    inline constexpr Simd::uint16_8 K{Long, Short, Long, 0, 0, 0, 0, 0};
-    inline constexpr Simd::uint16_8 L{Short, Long, Short, Short, 0, 0, 0, 0};
-    inline constexpr Simd::uint16_8 M{Long, Long, 0, 0, 0, 0, 0, 0};
-    inline constexpr Simd::uint16_8 N{Long, Short, 0, 0, 0, 0, 0, 0};
-    inline constexpr Simd::uint16_8 O{Long, Long, Long, 0, 0, 0, 0, 0};
-    inline constexpr Simd::uint16_8 P{Short, Long, Long, Short, 0, 0, 0, 0};
-    inline constexpr Simd::uint16_8 Q{Long, Long, Short, Long, 0, 0, 0, 0};
-    inline constexpr Simd::uint16_8 R{Short, Long, Short, 0, 0, 0, 0, 0};
-    inline constexpr Simd::uint16_8 S{Short, Short, Short, 0, 0, 0, 0, 0};
-    inline constexpr Simd::uint16_8 T{Long, 0, 0, 0, 0, 0, 0, 0};
-    inline constexpr Simd::uint16_8 U{Short, Short, Long, 0, 0, 0, 0, 0};
-    inline constexpr Simd::uint16_8 V{Short, Short, Short, Long, 0, 0, 0, 0};
-    inline constexpr Simd::uint16_8 W{Short, Long, Long, 0, 0, 0, 0, 0};
-    inline constexpr Simd::uint16_8 X{Long, Short, Short, Long, 0, 0, 0, 0};
-    inline constexpr Simd::uint16_8 Y{Long, Short, Long, Long, 0, 0, 0, 0};
-    inline constexpr Simd::uint16_8 Z{Long, Long, Short, Short, 0, 0, 0, 0};
+    inline constexpr Simd::int16_8 A{Short, Long, 0, 0, 0, 0, 0, 0};
+    inline constexpr Simd::int16_8 B{Long, Short, Short, Short, 0, 0, 0, 0};
+    inline constexpr Simd::int16_8 C{Long, Short, Long, Short, 0, 0, 0, 0};
+    inline constexpr Simd::int16_8 D{Long, Short, Short, 0, 0, 0, 0, 0};
+    inline constexpr Simd::int16_8 E{Short, 0, 0, 0, 0, 0, 0, 0};
+    inline constexpr Simd::int16_8 F{Short, Short, Long, Short, 0, 0, 0, 0};
+    inline constexpr Simd::int16_8 G{Long, Long, Short, 0, 0, 0, 0, 0};
+    inline constexpr Simd::int16_8 H{Short, Short, Short, Short, 0, 0, 0, 0};
+    inline constexpr Simd::int16_8 I{Short, Short, 0, 0, 0, 0, 0, 0};
+    inline constexpr Simd::int16_8 J{Short, Long, Long, Long, 0, 0, 0, 0};
+    inline constexpr Simd::int16_8 K{Long, Short, Long, 0, 0, 0, 0, 0};
+    inline constexpr Simd::int16_8 L{Short, Long, Short, Short, 0, 0, 0, 0};
+    inline constexpr Simd::int16_8 M{Long, Long, 0, 0, 0, 0, 0, 0};
+    inline constexpr Simd::int16_8 N{Long, Short, 0, 0, 0, 0, 0, 0};
+    inline constexpr Simd::int16_8 O{Long, Long, Long, 0, 0, 0, 0, 0};
+    inline constexpr Simd::int16_8 P{Short, Long, Long, Short, 0, 0, 0, 0};
+    inline constexpr Simd::int16_8 Q{Long, Long, Short, Long, 0, 0, 0, 0};
+    inline constexpr Simd::int16_8 R{Short, Long, Short, 0, 0, 0, 0, 0};
+    inline constexpr Simd::int16_8 S{Short, Short, Short, 0, 0, 0, 0, 0};
+    inline constexpr Simd::int16_8 T{Long, 0, 0, 0, 0, 0, 0, 0};
+    inline constexpr Simd::int16_8 U{Short, Short, Long, 0, 0, 0, 0, 0};
+    inline constexpr Simd::int16_8 V{Short, Short, Short, Long, 0, 0, 0, 0};
+    inline constexpr Simd::int16_8 W{Short, Long, Long, 0, 0, 0, 0, 0};
+    inline constexpr Simd::int16_8 X{Long, Short, Short, Long, 0, 0, 0, 0};
+    inline constexpr Simd::int16_8 Y{Long, Short, Long, Long, 0, 0, 0, 0};
+    inline constexpr Simd::int16_8 Z{Long, Long, Short, Short, 0, 0, 0, 0};
 
-    inline constexpr Simd::uint16_8 Zero{Long, Long, Long, Long, Long, 0, 0, 0};
-    inline constexpr Simd::uint16_8 One{Short, Long, Long, Long, Long, 0, 0, 0};
-    inline constexpr Simd::uint16_8 Two{Short, Short, Long, Long, Long, 0, 0, 0};
-    inline constexpr Simd::uint16_8 Three{Short, Short, Short, Long, Long, 0, 0, 0};
-    inline constexpr Simd::uint16_8 Four{Short, Short, Short, Short, Long, 0, 0, 0};
-    inline constexpr Simd::uint16_8 Five{Short, Short, Short, Short, Short, 0, 0, 0};
-    inline constexpr Simd::uint16_8 Six{Long, Short, Short, Short, Short, 0, 0, 0};
-    inline constexpr Simd::uint16_8 Seven{Long, Long, Short, Short, Short, 0, 0, 0};
-    inline constexpr Simd::uint16_8 Eight{Long, Long, Long, Long, Short, 0, 0, 0};
-    inline constexpr Simd::uint16_8 Nine{Long, Long, Long, Long, Long, 0, 0, 0};
+    inline constexpr Simd::int16_8 Zero{Long, Long, Long, Long, Long, 0, 0, 0};
+    inline constexpr Simd::int16_8 One{Short, Long, Long, Long, Long, 0, 0, 0};
+    inline constexpr Simd::int16_8 Two{Short, Short, Long, Long, Long, 0, 0, 0};
+    inline constexpr Simd::int16_8 Three{Short, Short, Short, Long, Long, 0, 0, 0};
+    inline constexpr Simd::int16_8 Four{Short, Short, Short, Short, Long, 0, 0, 0};
+    inline constexpr Simd::int16_8 Five{Short, Short, Short, Short, Short, 0, 0, 0};
+    inline constexpr Simd::int16_8 Six{Long, Short, Short, Short, Short, 0, 0, 0};
+    inline constexpr Simd::int16_8 Seven{Long, Long, Short, Short, Short, 0, 0, 0};
+    inline constexpr Simd::int16_8 Eight{Long, Long, Long, Long, Short, 0, 0, 0};
+    inline constexpr Simd::int16_8 Nine{Long, Long, Long, Long, Long, 0, 0, 0};
 }
 
-ByteVector ReadFile(const std::string& PathToFile)
+std::vector<char> DecodeMorseToPlainText(const std::string& PathToFile)
 {
-    std::fstream FStream{};
-
-    std::vector<uint8> CharArray{};
-
-    FStream.open(PathToFile, std::ios::in);
-
-    if(!FStream)
+    constexpr auto GetCharacterFromMorse = [](const Simd::int16_8& MorseCode) -> char
     {
-        std::cerr << "Failed to open file with path: " << PathToFile << std::endl;
-        return CharArray;
-    }
+        using namespace MorseCodes;
 
-    while(!FStream.eof())
-    {
-        uint8 TempChar;
-        FStream >> std::noskipws >> TempChar;
+        if(MorseCode == A) return 'A';
+        else if(MorseCode == B) return 'B';
+        else if(MorseCode == C) return 'C';
+        else if(MorseCode == D) return 'D';
+        else if(MorseCode == E) return 'E';
+        else if(MorseCode == F) return 'F';
+        else if(MorseCode == G) return 'G';
+        else if(MorseCode == H) return 'H';
+        else if(MorseCode == I) return 'I';
+        else if(MorseCode == J) return 'J';
+        else if(MorseCode == K) return 'K';
+        else if(MorseCode == L) return 'L';
+        else if(MorseCode == M) return 'M';
+        else if(MorseCode == N) return 'N';
+        else if(MorseCode == O) return 'O';
+        else if(MorseCode == P) return 'P';
+        else if(MorseCode == Q) return 'Q';
+        else if(MorseCode == R) return 'R';
+        else if(MorseCode == S) return 'S';
+        else if(MorseCode == T) return 'T';
+        else if(MorseCode == U) return 'U';
+        else if(MorseCode == V) return 'V';
+        else if(MorseCode == W) return 'W';
+        else if(MorseCode == X) return 'X';
+        else if(MorseCode == Y) return 'Y';
+        else if(MorseCode == Z) return 'Z';
+        else if(MorseCode == Zero) return '0';
+        else if(MorseCode == One) return '1';
+        else if(MorseCode == Two) return '2';
+        else if(MorseCode == Three) return '3';
+        else if(MorseCode == Four) return '4';
+        else if(MorseCode == Five) return '5';
+        else if(MorseCode == Six) return '6';
+        else if(MorseCode == Seven) return '7';
+        else if(MorseCode == Eight) return '8';
+        else if(MorseCode == Nine) return '9';
+        else if(MorseCode == NewWordChar) return ' ';
+        else if(MorseCode == NullChar) return '#';
 
-        CharArray.emplace_back(TempChar);
-    }
-
-    FStream.close();
-
-    return CharArray;
-}
-
-ByteVector DecodeMorseToPlainText(const std::string& PathToFile)
-{
-    Simd::uint16_8 Code{static_cast<uint16>(0)};
-    uint8 CodeIndex{0};
-
-    ByteVector PlainTextString{};
-
-    auto PushBackChar = [&CodeIndex, &Code, &PlainTextString]() -> void
-    {
-        auto GetCharacterFromMorse = [](const Simd::uint16_8& MorseCode) -> char
-        {
-            using namespace MorseCodes;
-
-            if(MorseCode == A) return 'A';
-            else if(MorseCode == B) return 'B';
-            else if(MorseCode == C) return 'C';
-            else if(MorseCode == D) return 'D';
-            else if(MorseCode == E) return 'E';
-            else if(MorseCode == F) return 'F';
-            else if(MorseCode == G) return 'G';
-            else if(MorseCode == H) return 'H';
-            else if(MorseCode == I) return 'I';
-            else if(MorseCode == J) return 'J';
-            else if(MorseCode == K) return 'K';
-            else if(MorseCode == L) return 'L';
-            else if(MorseCode == M) return 'M';
-            else if(MorseCode == N) return 'N';
-            else if(MorseCode == O) return 'O';
-            else if(MorseCode == P) return 'P';
-            else if(MorseCode == Q) return 'Q';
-            else if(MorseCode == R) return 'R';
-            else if(MorseCode == S) return 'S';
-            else if(MorseCode == T) return 'T';
-            else if(MorseCode == U) return 'U';
-            else if(MorseCode == V) return 'V';
-            else if(MorseCode == W) return 'W';
-            else if(MorseCode == X) return 'X';
-            else if(MorseCode == Y) return 'Y';
-            else if(MorseCode == Z) return 'Z';
-            else if(MorseCode == Zero) return '0';
-            else if(MorseCode == One) return '1';
-            else if(MorseCode == Two) return '2';
-            else if(MorseCode == Three) return '3';
-            else if(MorseCode == Four) return '4';
-            else if(MorseCode == Five) return '5';
-            else if(MorseCode == Six) return '6';
-            else if(MorseCode == Seven) return '7';
-            else if(MorseCode == Eight) return '8';
-            else if(MorseCode == Nine) return '9';
-
-            return '#';
-        };
-
-        PlainTextString.push_back(GetCharacterFromMorse(Code));
-
-        Code = static_cast<uint16>(0);
-        CodeIndex = 0;
+        return '#';
     };
 
-    for(const uint8 Character : ReadFile(PathToFile))
+    auto DecodeFile = [&GetCharacterFromMorse, &PathToFile]() -> std::vector<char>
     {
-        if unlikely(Character == MorseCodes::Space)
-        {
-            PushBackChar();
-        }
-        else if unlikely(Character == MorseCodes::NewWord)
-        {
-            PushBackChar();
-            PlainTextString.push_back(' ');
-        }
-        else
-        {
-            Code.Get()[CodeIndex] = static_cast<uint16>(Character);
-            ++CodeIndex;
-        }
-    }
+        std::fstream FStream{};
 
-    return PlainTextString;
+        FStream.open(PathToFile, std::ios::in);
+
+        std::vector<char> PlainTextVector{};
+
+        if(!FStream)
+        {
+            std::cerr << "Failed to open file with path: " << PathToFile << std::endl;
+            return PlainTextVector;
+        }
+
+        Simd::int16_8 MorseCode{};
+
+        for(int8 RegisterIndex{0}; !FStream.eof(); ++RegisterIndex)
+        {
+            char TempChar;
+            FStream >> std::noskipws >> TempChar;
+
+            if unlikely(TempChar == static_cast<char>(MorseCodes::Space))
+            {
+                PlainTextVector.emplace_back(GetCharacterFromMorse(MorseCode));
+
+                MorseCode = static_cast<int16>(0);
+                RegisterIndex = -1;
+            }
+            else if unlikely(TempChar == static_cast<char>(MorseCodes::NewWord))
+            {
+                PlainTextVector.emplace_back(GetCharacterFromMorse(MorseCode));
+                PlainTextVector.emplace_back(' ');
+
+                MorseCode = static_cast<int16>(0);
+                RegisterIndex = -1;
+            }
+            else
+            {
+                check(RegisterIndex < 8)
+                MorseCode.Register[RegisterIndex] = static_cast<int16>(TempChar);
+            }
+        }
+
+        FStream.close();
+
+        return PlainTextVector;
+    };
+
+    return DecodeFile();
 }
 
-std::vector<Simd::uint16_8> EncodePlainTextToMorse(const std::string& PathToFile)
+std::vector<Simd::int16_8> EncodePlainTextToMorse(const std::string& PathToFile)
 {
-    constexpr auto GetMorseFromCharacter = [](uint8 Character) -> Simd::uint16_8
+    constexpr auto GetMorseFromCharacter = [](char Character) -> Simd::int16_8
     {
         using namespace MorseCodes;
 
@@ -220,24 +212,35 @@ std::vector<Simd::uint16_8> EncodePlainTextToMorse(const std::string& PathToFile
         }
     };
 
-    std::vector<Simd::uint16_8> MorseCodeVector{};
+    std::fstream FStream{};
 
-    const ByteVector& PlainCharacterVector{ReadFile(PathToFile)};
+    FStream.open(PathToFile, std::ios::in);
 
-    MorseCodeVector.reserve(PlainCharacterVector.size() * Simd::uint16_8::GetNumElements());
+    std::vector<Simd::int16_8> MorseCodeVector{};
 
-    for(uint64 Index{0}; Index < PlainCharacterVector.size() - 1; ++Index)
+    if(!FStream)
     {
-        if unlikely(MorseCodeVector.emplace_back(GetMorseFromCharacter(PlainCharacterVector[Index])) != MorseCodes::NewWordChar)
+        std::cerr << "Failed to open file with path: " << PathToFile << std::endl;
+        return MorseCodeVector;
+    }
+
+    while(!FStream.eof())
+    {
+        char TempChar;
+        FStream >> std::noskipws >> TempChar;
+
+        if(MorseCodeVector.emplace_back(GetMorseFromCharacter(TempChar)) != MorseCodes::NewWordChar)
         {
-            MorseCodeVector.push_back(MorseCodes::SeparateCharacterChar);
+            MorseCodeVector.emplace_back(MorseCodes::SeparateCharacterChar);
         }
     }
+
+    FStream.close();
 
     return MorseCodeVector;
 }
 
-void WriteToFile(const std::string& PathToOutFile, const ByteVector& StringToWrite)
+void WriteToFile(const std::string& PathToOutFile, const std::vector<char>& StringToWrite)
 {
     std::fstream FStream{};
 
@@ -251,16 +254,13 @@ void WriteToFile(const std::string& PathToOutFile, const ByteVector& StringToWri
 
     for(const uint8 Character : StringToWrite)
     {
-        if(Character != '#')
-        {
-            FStream << Character;
-        }
+        FStream << Character;
     }
 
     FStream.close();
 }
 
-void WriteToFile(const std::string& PathToOutFile, const std::vector<Simd::uint16_8>& StringToWrite)
+void WriteToFile(const std::string& PathToOutFile, const std::vector<Simd::int16_8>& StringToWrite)
 {
     std::fstream FStream{};
 
@@ -276,7 +276,6 @@ void WriteToFile(const std::string& PathToOutFile, const std::vector<Simd::uint1
     {
         FStream << static_cast<char>(Character);
     });
-
 
     FStream.close();
 }
